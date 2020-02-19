93c382e2ebc45920431224aad26b3c479d11a365
acid_utopia <g.aaliyah1016@gmail.com>
2020-02-19 15:59:11 +0000

    added average func

--- nimbot.py
+++ nimbot.py
@@ -12,6 +12,9 @@ bot = commands.Bot(command_prefix='!')
 async def play_nim(ctx):
     await ctx.send("Still working on it, kiddo.")
 
+@bot.command(name= "average")
+async def average(ctx,a,b):
+    await ctx.send((a+b)/2)
 bot.run(token)
 
 #Aaliyah Garcia
